6393de4514ac3384153f61d3278bc6029cf88a72
mhobi5511 <marc.hobi@otv.ch>
2026-05-07 03:05:50 +0200

    Align header logo with navigation

--- src/App.tsx
+++ src/App.tsx
@@ -291,13 +291,13 @@ function Header() {
 
   return (
     <header className="flex flex-col gap-3 lg:flex-row lg:items-center">
-      <div className="flex items-center justify-between lg:block lg:shrink-0">
+      <div className="flex items-center justify-between lg:flex lg:h-[76px] lg:shrink-0 lg:items-center">
           <Link href="/" className="inline-flex items-center" ariaLabel="Drums2Streets">
-          <span className="relative flex h-20 w-52 items-center justify-center sm:h-24 sm:w-64 lg:h-24 lg:w-72">
+          <span className="relative flex h-20 w-52 items-center justify-center sm:h-24 sm:w-64 lg:h-[76px] lg:w-72">
               <img
                 src={logoImage}
                 alt="Drums2Streets"
-              className="absolute h-28 w-72 max-w-none object-contain drop-shadow-2xl sm:h-36 sm:w-96 lg:h-40 lg:w-[28rem]"
+              className="absolute h-28 w-72 max-w-none object-contain sm:h-36 sm:w-96 lg:h-36 lg:w-[28rem]"
               />
             </span>
           </Link>
